912b6331e6037249a6b613770fd35fa199f55576
Hassansalm <132700607+Hassansalm@users.noreply.github.com>
2026-04-05 06:35:27 +0300

    Create singlecard.py

---
+++ singlecard.py
@@ -0,0 +1,14 @@
+from flet import *
+def create_image():
+    return Container(
+
+        Image(
+            src="assets/prodict/1.jpeg",
+            expand=True,
+            
+        ),
+        padding=0,
+        margin=0,
+        alignment=Alignment.CENTER
+
+     )
